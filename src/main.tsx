34a289c56916d69acc950c306e6a07a393f0d8cd
Rye Nguyen <ryentd.vn@gmail.com>
2024-04-14 08:31:04 +0700

    add use query conf

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { queryClient } from "@/utils/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
